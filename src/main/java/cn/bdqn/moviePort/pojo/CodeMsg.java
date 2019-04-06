77a97346b81a81eb5d74e1691c911207a884b865
ylm <1637653089@qq.com>
2019-04-07 01:11:37 +0800

    注册登录优化2...

--- src/main/java/cn/bdqn/moviePort/pojo/CodeMsg.java
+++ src/main/java/cn/bdqn/moviePort/pojo/CodeMsg.java
@@ -7,7 +7,7 @@ public class CodeMsg {
     private String message;
     private Map<String,Object> errorMap;
 
-    public static CodeMsg SUCCESS= new CodeMsg(0,"success");
+    public static CodeMsg SUCCESS= new CodeMsg(0,"SUCCESS");
     public static CodeMsg SERVER_EXCEPTION = new CodeMsg(500100,"服务端异常");
     public static CodeMsg PARAMETER_ISNULL = new CodeMsg(500101,"输入参数为空");
     public static CodeMsg USER_NOT_EXIST = new CodeMsg(500102,"用户不存在");
